d3ed5e3dad1a260601abdd20d22a8f95fe11c95c
janilton <janiltomaciel@gmail.com>
2009-08-02 18:49:10 -0300

    teste

--- ame/forms.py
+++ ame/forms.py
@@ -3,7 +3,7 @@ from django.contrib.admin.widgets import FilteredSelectMultiple
 from ame.models import Paciente, Estado, Pais
 
     #pais = ModelChoiceField(queryset=Pais.objects.all(), widget=FilteredSelectMultiple("aaa",False))
-#    estado = ModelChoiceField(queryset=Duplicata.objects.filter(Q(status='I')|Q(status='L')) 
+    #estado = ModelChoiceField(queryset=Duplicata.objects.filter(Q(status='I')|Q(status='L')) 
     #estado = ModelChoiceField(queryset=Estado.objects.all())
 
 
